ac352939db1ab85def4d43402ea4c88ca93f5c81
Shantanu Godbole <46984743+shantanugodbole@users.noreply.github.com>
2021-03-30 10:03:48 +0530

    minor change

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Testing Jenkins and Dcoker Integration" );
+        System.out.println( "Testing Jenkins and Docker Integration" );
     }
 }
